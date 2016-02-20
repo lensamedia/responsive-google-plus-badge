fa806208957932f10e963d26e257ae3787f1b829
Mentarimedia <mastermentarimedia@gmail.com>
2016-02-20 10:07:32 +0700

    fix

--- js/google-badge-responsive.js
+++ js/google-badge-responsive.js
@@ -1,9 +1,12 @@
 $(document).ready(function() {
+   var id = '111746071444691234996'; // change this with your google plus id;//
    var w=$(".gplusbadage").width();
-   $(".gplusbadage").html('<script src="https://apis.google.com/js/platform.js" async defer>{parsetags: \'explicit\'}</script><div class="g-page" data-href="//plus.google.com/u/0/111746071444691234996" data-width="'+ w +'" data-rel="publisher"></div><script type="text/javascript">gapi.page.go();</script>');
+   var gurl = '<script src="https://apis.google.com/js/platform.js" async defer>{parsetags: \'explicit\'}</script><div class="g-page" data-href="//plus.google.com/u/0/'+ id +'" data-width="'+ w +'" data-rel="publisher"></div><script type="text/javascript">gapi.page.go();</script>';
+   $(".gplusbadage").html(gurl);
    $(window).resize(function(){
-     w = $(".gplusbadage").width();
-    $(".gplusbadage").empty();
-    $(".gplusbadage").html('<script src="https://apis.google.com/js/platform.js" async defer>{parsetags: \'explicit\'}</script><div class="g-page" data-href="//plus.google.com/u/0/111746071444691234996" data-width="'+ w +'" data-rel="publisher"></div><script type="text/javascript">gapi.page.go();</script>');
+   w=$(".gplusbadage").width();
+   gurl = '<script src="https://apis.google.com/js/platform.js" async defer>{parsetags: \'explicit\'}</script><div class="g-page" data-href="//plus.google.com/u/0/'+ id +'" data-width="'+ w +'" data-rel="publisher"></div><script type="text/javascript">gapi.page.go();</script>';
+   $(".gplusbadage").empty();
+   $(".gplusbadage").html(gurl);
 });
 });
